f6e4402bfd583ee1ca85d192cdf6aad13fe32a43
Jesus Lozano <jesus.lozano@adm.interactiveworkers.com>
2025-12-15 12:16:47 -0400

    Initial UI

---
+++ LiveLocalCaptions/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,56 @@
+﻿using LiveLocalCaptions.Classes;
+
+namespace LiveLocalCaptions.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting
+    {
+        get => _Greeting;
+        set
+        {
+            if (_Greeting != value)
+            {
+                _Greeting = value;                
+                OnPropertyChanged();
+            }
+        }
+    } 
+    private string _Greeting { get; set; } = "Live Captions will appear here";
+
+    public string StatusButton
+    {
+        get => _StatusButton;
+        set
+        {
+            if (_StatusButton != value)
+            {
+                _StatusButton = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+    private string _StatusButton { get; set; } = "Start";
+    
+    public RelayCommand StartCommand { get; }
+
+    public MainWindowViewModel()
+    {
+        StartCommand = new RelayCommand(
+            execute: _ => Start(),
+            canExecute: _ => true
+        );
+    }
+    
+    public void Start()
+    {
+        if (StatusButton == "Start")
+        {
+            StatusButton = "Stop";
+        }
+        else
+        {
+            StatusButton = "Start";
+        }
+    }
+}
