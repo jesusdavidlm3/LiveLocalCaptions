0487709623865b9a4aa6860730df31d7a34294f1
Jesus Lozano <jesus.lozano@adm.interactiveworkers.com>
2026-01-23 09:55:45 -0400

    Correcciones al calcular tamaño el buffer, resampler declarado

--- test/Program.cs
+++ test/Program.cs
@@ -18,54 +18,55 @@ using var processor = whisperFactory.CreateBuilder()
 //Configuracion de la captura de audio
 using var capture = new WasapiLoopbackCapture();
 var bufferStream = new MemoryStream();
-int bytesPerSecond = 16000 * 2;
+int bytesPerSecond = 48000 * 2 * 2;     //samples por segundo, profundidad en bytes, canales
 int segmentDurationSeconds = 5;
 int segmentSize = bytesPerSecond * segmentDurationSeconds;
 
 //Ajustes de formato
-var waveFormat = new WaveFormat(48000, 24, 2 );
-var writer = new WaveFileWriter("test.wav", waveFormat);
-// var targetFormat = new WaveFormat(16000, 16, 1);
-// var bufferedWaveProvicer = new BufferedWaveProvider(capture.WaveFormat);
-// var resampler = new MediaFoundationResampler(bufferedWaveProvicer, targetFormat)
-// {
-//     ResamplerQuality = 60
-// };
-capture.WaveFormat = waveFormat; 
+var sourceFormat = new WaveFormat(48000, 16, 2 );
+var targetFormat = new WaveFormat(16000, 16, 1);
+var writer = new WaveFileWriter("test.wav", targetFormat);
+capture.WaveFormat = sourceFormat;
+var bufferedWaveProvider = new BufferedWaveProvider(sourceFormat);
+bufferedWaveProvider.BufferLength = segmentSize * 2;
 
 //Operaciones a realizar durante la captura
 capture.DataAvailable += async (s, e) =>
 {
-    // bufferStream.Write(e.Buffer, 0, e.BytesRecorded);
-    // if (bufferStream.Length >= segmentSize)
-    // {
-        //Procesador de audio capturado
-        // var audioBytes =  bufferStream.ToArray();
+    if (bufferedWaveProvider.BufferedBytes < segmentSize)
+    {
+        bufferedWaveProvider.AddSamples(e.Buffer, 0, e.BytesRecorded);
+    }
+    else
+    {
+        //Resamplear a 16KHz, 16 bits, mono
+        
+        var resampler = new MediaFoundationResampler(bufferedWaveProvider, targetFormat);
+        resampler.ResamplerQuality = 60;
+        
+        // var audioBytes =  bufferedWaveProvicer;
         // bufferStream.SetLength(0);
-        // float[] samples = new float[audioBytes.Length / 2];
+        // float[] samples = new float[bufferedWaveProvicer.BufferedBytes];
         // for (int i = 0; i < samples.Length; i++)
         // {
         //     short sample = BitConverter.ToInt16(audioBytes, i * 2);
         //     samples[i] = sample / 32768f;
-        //     writer.Write(e.Buffer, 0, e.BytesRecorded);
         // }
 
         //Modelo de transcripcion
-        // try
-        // {
-        //     await foreach (var result in processor.ProcessAsync(samples))
-        //     {
-        //         Console.WriteLine($"{result.Start}->{result.End}: {result.Text}");
-        //     }            
-        // }catch(Exception ex)
-        // {
-        //     Console.WriteLine($"No hay audio para Capturar");
-        // }
-        
-        //Guardado de audio en archivo
-        // writer.Write(e.Buffer, 0, e.BytesRecorded);
-    // }
-    writer.Write(e.Buffer, 0, e.BytesRecorded);
+        try
+        {
+            // await foreach (var result in processor.ProcessAsync(samples)) 
+            // {
+            //     Console.WriteLine($"{result.Start}->{result.End}: {result.Text}");
+            // }            
+        }catch(Exception ex)
+        {
+            Console.WriteLine($"No hay audio para Capturar");
+        }
+    }
+    //Guardado de audio en archivo
+    // writer.Write(e.Buffer, 0, e.BytesRecorded);   no se donde va esto...
 };
 
 //Operaciones al terminar la captura
